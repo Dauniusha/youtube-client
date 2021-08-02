32eed3c5ab7d6eb7e466db8b4356cb4bd7bf3fd0
Dauniusha <78425928+Dauniusha@users.noreply.github.com>
2021-08-02 10:31:22 +0300

    fix: injection problem

--- youtube-client/src/app/main/components/mini-card/mini-card.component.ts
+++ youtube-client/src/app/main/components/mini-card/mini-card.component.ts
@@ -8,18 +8,23 @@ import { CardStatistics } from '../../models/card-statistics-interface';
   styleUrls: ['./mini-card.component.scss'],
 })
 export class MiniCardComponent implements OnInit {
-  private id: string;
+  private id: string = '';
 
-  private title: string;
+  private title: string = '';
 
-  private statistics: CardStatistics;
+  private statistics: CardStatistics = {
+    likeCount: 0,
+    viewCount: 0,
+    dislikeCount: 0,
+    commentCount: 0,
+  };
 
   private color: string = '';
 
-  constructor(data: CardData) {
-    this.id = data.id;
-    this.title = data.title;
-    this.statistics = data.statistics;
+  constructor(/* data: CardData */) {
+    // this.id = data.id;
+    // this.title = data.title;
+    // this.statistics = data.statistics;
   }
 
   ngOnInit(): void {
